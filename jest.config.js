077f4c82c1b40283bfd1c632d4f5b7e13fa1d6e0
mahmoudshahin1111@gmail.com <mahmoudshahin1111@gmail.com>
2021-09-24 23:13:01 +0200

    remove app component of testing

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
     testPathIgnorePatterns:[
         "<rootDir>/src/test.ts",
+        "<rootDir>/src/app/app.component.spec.ts",
         "<rootDir>/projects/ngx-cxl-popover/src/test.ts"
     ]
   };
