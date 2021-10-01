e0691459f349743f1bd5eb74301bec1e65345eb2
mahmoudshahin1111@gmail.com <mahmoudshahin1111@gmail.com>
2021-10-01 19:46:10 +0200

    fixing animation bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgxCxlPopoverModule } from 'projects/ngx-cxl-popover/src/public-api';
+import { NgxCxlPopoverModule } from 'ngx-cxl-popover';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
